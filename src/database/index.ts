5e72644f5d11920564ba0ef44932a3a5cfeb4166
odm275 <pomejia@gmail.com>
2020-09-06 13:54:20 -0500

    fix env variable name

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,10 @@
 import { MongoClient } from 'mongodb'
 import { Database } from '../lib/types'
+console.log(process.env.DB_USER)
+console.log(process.env.DB_USER_PASSWORD)
+console.log(process.env.DB_CLUSTER)
 
-const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`
+const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@${process.env.DB_CLUSTER}.mongodb.net/<dbname>?retryWrites=true&w=majority`
 
 export const connectDatabase = async (): Promise<Database> => {
   const client = await MongoClient.connect(url, {
