5df83d37fa0651b334175520a54ad3c69b85b7de
odm275 <pomejia@gmail.com>
2020-10-02 23:23:02 -0500

    listings and bookings for user page

--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,12 @@
 import merge from "lodash.merge";
 import { userResolvers } from "./User";
 import { viewerResolvers } from "./Viewer";
+import { listingResolvers } from "./Listing";
+import { bookingResolvers } from "./Booking";
 
-export const resolvers = merge(viewerResolvers, userResolvers);
+export const resolvers = merge(
+  bookingResolvers,
+  listingResolvers,
+  viewerResolvers,
+  userResolvers
+);
